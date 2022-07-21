d30b56aee7fe7b92f01ce749c2ba70df2c9b151f
Kseniya Tarasevich <tarasevichkseniya11@gmail.com>
2022-07-21 23:00:51 +0300

    i'm trying make sound volume saving on both scenes

--- MyFirstMob/Assets/Scripts/SliderController.cs
+++ MyFirstMob/Assets/Scripts/SliderController.cs
@@ -10,14 +10,25 @@ public class SliderController : MonoBehaviour
 
     private void Start()
     {
-        
+        oldVolume = slider.value;
+        if (!PlayerPrefs.HasKey("volumeValue"))
+        {
+            slider.value = 0.1f;
+        }
+
+        else
+        {
+            slider.value = PlayerPrefs.GetFloat("volumeValue");
+        }
     }
 
     private void Update()
     {
         if (oldVolume != slider.value)
         {
-
+            PlayerPrefs.SetFloat("volumeValue", slider.value);
+            PlayerPrefs.Save();
+            oldVolume = slider.value;
         }
     }
 }
