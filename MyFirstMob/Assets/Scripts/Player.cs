a142f92f627c4bce8a29a52364a4697db8cd4f05
Kseniya Tarasevich <tarasevichkseniya11@gmail.com>
2022-07-23 14:01:25 +0300

    ...

--- MyFirstMob/Assets/Scripts/Player.cs
+++ MyFirstMob/Assets/Scripts/Player.cs
@@ -16,24 +16,27 @@ public class Player : MonoBehaviour
 
 
 
-    void Awake() {
+    void Awake()
+    {
         lose = false;
         i = 0;
         score = 0;
     }
 
-    void OnTriggerEnter2D(Collider2D other) {
-        
-    if(other.gameObject.tag == "Brick")
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.tag == "Brick")
         {
             soundBrick.Play();
             i++;
 
             FallDown.falls = 0f;
 
-            if (i>=1) { heart1.SetActive(lose); }
-            if (i>=2) { heart2.SetActive(lose); }
-            if (i >= 3) {
+            if (i >= 1) { heart1.SetActive(lose); }
+            if (i >= 2) { heart2.SetActive(lose); }
+            if (i >= 3)
+            {
                 heart3.SetActive(lose);
                 lose = true;
                 restart.SetActive(lose);
@@ -43,11 +46,12 @@ public class Player : MonoBehaviour
 
         if (other.gameObject.tag == "Coin")
         {
-            
+
             soundCoin.Play();
             score++;
 
-            if (score % 20 == 0) {
+            if (score % 20 == 0)
+            {
                 FallDown.falls += 0.5f;
             }
 
